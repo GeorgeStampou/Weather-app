82f22ec49863cf1baba4134e244ddb40500a6685
George Stampoulidis <georgios.stampoulidis@gmail.com>
2023-09-18 15:34:26 +0300

    handles error messages, added no location icon

--- script.js
+++ script.js
@@ -6,37 +6,48 @@ const tempC = document.querySelector(".temp");
 const city = document.querySelector(".city");
 const wind = document.querySelector(".wind-speed");
 const hum = document.querySelector(".hum");
+const containerError = document.querySelector(".container-error");
+const errorMessage = document.querySelector(".error-message");
 
 
 const APIKey = "79b221dd0def4426838194006231609";
 
 
 
+
 btn.addEventListener("click", onClick);
 
 function onClick(){
+    if(containerError.classList.contains("container-error-active")){
+        containerError.classList.remove("container-error-active");
+    }
     if(input.reportValidity()){
         containerInfo.classList.add("container-info-active");
         const cityToSearch = input.value;
         fetch(`http://api.weatherapi.com/v1/current.json?key=${APIKey}&q=${cityToSearch}`)
             .then(res=>{
-                console.log(res.status);
+                /*console.log(res.status);*/
                 if(res.ok){
                     return res.json();
                 }else{
-                    throw new Error("Network Error");
+                    return res.json();
                 }
                 /* handle errors with res.status.  */
             })
             .then(data=>{
-                console.log(data)
-                weatherimg.src = data.current.condition.icon;
-                weatherimg.alt = data.current.condition.text;
-                tempC.innerHTML = Math.round(data.current.temp_c) + "°C";
-                city.innerHTML = data.location.name;
-                wind.innerHTML = data.current.wind_kph + "km/h";
-                hum.innerHTML = data.current.humidity + "%";
-
+                console.log(data);
+                if( data.error != undefined && "code" in data.error && "message" in data.error){
+                    containerInfo.classList.remove("container-info-active");
+                    containerError.classList.add("container-error-active");
+                    errorMessage.innerHTML = data.error.message;
+                }else{
+                    weatherimg.src = data.current.condition.icon;
+                    weatherimg.alt = data.current.condition.text;
+                    tempC.innerHTML = Math.round(data.current.temp_c) + "°C";
+                    city.innerHTML = data.location.name;
+                    wind.innerHTML = data.current.wind_kph + "km/h";
+                    hum.innerHTML = data.current.humidity + "%";
+                }
                 
             })
             .catch(error=>{
